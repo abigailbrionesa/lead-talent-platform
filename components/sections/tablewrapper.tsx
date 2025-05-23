9584f201f401b32ad4228fd97e3c586743c044bb
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-23 06:41:04 -0500

    add new rows to tablewrapper

--- components/sections/tablewrapper.tsx
+++ components/sections/tablewrapper.tsx
@@ -5,16 +5,22 @@ import { supabase } from '@/lib/supabase'
 import { DataTable } from '@/components/sections/table'
 import { columns } from '@/components/sections/columns'
 
-export type FormResponses = {
-  id: number;
-  name?: string | null;
-  email?: string | null;
-  age?: number | null;
-  submitted_at?: Date | null;
-};
+export interface FormResponse {
+  name: string;
+  email: string;
+  age: number;
+  phone: string;
+  chapter: string;
+  role: string;
+  university_cycle: string;
+  career: string;
+  availability: string;
+  linkedin_url: string;
+  resume_url: string;
+}
 
 export default function DataTableWrapper() {
-  const [data, setData] = useState<FormResponses[]>([])
+  const [data, setData] = useState<FormResponse[]>([])
 
   const fetchData = async () => {
     const { data, error } = await supabase.from('form_responses').select('*')
