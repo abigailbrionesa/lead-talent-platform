6395ccb5264537f32883f4a12703ef15c5b70239
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-24 09:31:55 -0500

    user button

---
+++ components/ui/user-button.tsx
@@ -0,0 +1,43 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar"
+import { Button } from "./button"
+import { auth } from "@/auth"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./dropdown-menu"
+import SignIn from "./sign-in"
+import SignOut from "./sign-out"
+import type { Session } from "next-auth"
+
+export default function UserButton({session}:{session:Session | null}) {
+
+if (!session?.user) return <SignIn />
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative w-8 h-8 rounded-full">
+          <Avatar className="w-8 h-8">
+            <AvatarImage
+              src={session.user.image ?? "https://source.boringavatars.com/beam/120"}
+              alt={session.user.name ?? ""}
+            />
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{session.user.name}</p>
+            <p className="text-xs leading-none text-muted-foreground">{session.user.email}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuItem>
+          <SignOut />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
