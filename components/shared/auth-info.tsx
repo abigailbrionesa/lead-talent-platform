f5c9a122138b8e8ef6cb755426faed22bd6433b5
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-27 15:30:11 -0500

    show all auth info

--- components/shared/auth-info.tsx
+++ components/shared/auth-info.tsx
@@ -1,9 +1,28 @@
-import { auth } from '@/auth';
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
 
 export default async function AuthInfo() {
   const session = await auth();
 
+  if (!session?.user?.id) {
+    return <div className="p-8">Not logged in</div>;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+    include: {
+      member: true,
+      recruiter: true,
+    },
+  });
+
   return (
-    <pre className="p-8">{JSON.stringify(session, null, 2)}</pre>
+    <div className="p-8">
+      <h1 className="text-xl font-bold">User Info</h1>
+      <pre className="mt-4">{JSON.stringify(user, null, 2)}</pre>
+
+    </div>
   );
 }
