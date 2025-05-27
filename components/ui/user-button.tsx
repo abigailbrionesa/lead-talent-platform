3d25adb71f033172981466610477760cfade4c3b
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-27 15:31:57 -0500

    user  button (Better ui)

--- components/ui/user-button.tsx
+++ components/ui/user-button.tsx
@@ -1,43 +1,92 @@
-import { Avatar, AvatarFallback, AvatarImage } from "./avatar"
-import { Button } from "./button"
-import { auth } from "@/auth"
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
-} from "./dropdown-menu"
-import SignIn from "./sign-in"
-import SignOut from "./sign-out"
-import type { Session } from "next-auth"
+  DropdownMenuSeparator,
+  DropdownMenuGroup,
+} from "./dropdown-menu";
+import Link from "next/link";
+import SignIn from "./sign-in";
+import type { User } from "next-auth";
+import {
+  BadgeCheck,
+  Bell,
+  ChevronsUpDown,
+  Settings,
+  LogOut,
+} from "lucide-react";
+import { Button } from "./button";
+import SignOut from "./sign-out";
+import { signOut } from "next-auth/react";
 
-export default function UserButton({session}:{session:Session | null}) {
+export default function UserButton({ user }: { user: User | undefined }) {
+  if (!user) return <SignIn />;
 
-if (!session?.user) return <SignIn />
   return (
-    <DropdownMenu>
+    <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-          <Avatar className="w-8 h-8">
-            <AvatarImage
-              src={session.user.image ?? "https://source.boringavatars.com/beam/120"}
-              alt={session.user.name ?? ""}
-            />
+        <Button size="lg" variant={"ghost"} className="">
+          <Avatar className="h-8 w-8 rounded-lg">
+            <AvatarImage src={user.image ?? "/placeholder.svg"} />
+            <AvatarFallback className="rounded-lg">CN</AvatarFallback>
           </Avatar>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-semibold">{user.name}</span>
+            <span className="truncate text-xs">{user.role}</span>
+          </div>
+          <ChevronsUpDown className="ml-auto size-4" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{session.user.name}</p>
-            <p className="text-xs leading-none text-muted-foreground">{session.user.email}</p>
+      <DropdownMenuContent
+        className="min-w-56 rounded-lg bg-background"
+        side={"bottom"}
+        align="end"
+      >
+        <DropdownMenuLabel className="p-0 font-normal">
+          <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+            <Avatar className="h-8 w-8 rounded-lg">
+              <AvatarImage src={user.image ?? "/placeholder.svg"} />
+              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+            </Avatar>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-semibold">{user.name}</span>
+              <span className="truncate text-xs">{user.email}</span>
+            </div>
           </div>
         </DropdownMenuLabel>
-        <DropdownMenuItem>
-          <SignOut />
+        <DropdownMenuSeparator />
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <Link
+            href="/account"
+            passHref
+            className="cursor-pointer
+          
+          focus:bg-slate-100 focus:text-slate-900 
+          
+          
+        relative flex items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none  [&_svg]:pointer-events-none [&_svg]:shrink-0
+         [&_svg:not([class*='size-'])]:size-4 dark:focus:bg-slate-800 dark:focus:text-slate-50
+         [&_svg:not([class*='text-'])]:text-slate-500 
+       dark:[&_svg:not([class*='text-'])]:text-slate-400"
+          >
+            <Settings /> Editar Perfil
+          </Link>
+
+          <DropdownMenuItem>
+            <Bell />
+            Notificaciones
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>
+          <LogOut />
+          Cerrar Sesión
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
