ba2b593adee99fe8f0151abcb2f2f4b1c4192e48
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-27 04:36:43 -0500

    change name

--- components/ui/sign-out.tsx
+++ components/ui/sign-out.tsx
@@ -2,7 +2,7 @@
 import { Button } from "./button";
 import { signOut } from "next-auth/react";
 
-export default function SignIn() {
+export default function SignOut() {
   return (
     <Button onClick={() => signOut()} size="sm" variant="outline">
       Sign out
