877095424bc3620669a6991b99df06314422bf46
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-28 08:40:03 -0500

    protect route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,24 @@
 import NavbarServer from "@/components/global/navbar-server";
 import AuthWrapper from "@/components/shared/auth-info"; 
 import DataTableWrapper from "@/components/sections/tablewrapper";
-export default function DashboardPage() {
+import { auth } from "@/auth";
+
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session) {
+    return <div>Not authenticated</div>;
+  }
+
+  if (session.user.role !== "RECRUITER") {
+    return <div>You do not have access to this database</div>;
+  }
+
   return (
     <div>
       <NavbarServer />
       <div className="h-20" />
       <DataTableWrapper />
-      <AuthWrapper />
     </div>
   );
 }
