3290097f3f25263f6a5bf4433b8722f1fb5d613c
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-31 04:34:43 -0500

    fix import path

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
-import LandingSection from "@/components/sections/landing";
 import { auth } from "@/auth";
 import { Navbar } from "@/components/global/navbar";
+import { LandingSection } from "@/components/sections/landing";
 
 export default async function Home() {
   const session = await auth();
