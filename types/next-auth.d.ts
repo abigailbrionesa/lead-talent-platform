0b6f7309f4f7f5eb5552783ae91c861995a59440
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-31 04:42:08 -0500

    fix types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,30 +1,24 @@
 import NextAuth from "next-auth";
-
+import type { Role } from "@prisma/client";
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-      role?: string | null;
+      name: string;
+      email: string;
+      image: string | null;
+      role: Role | null;
       emailVerified: Date | null;
       password: string | null;
-      isProfileComplete?: boolean;
+      isProfileComplete: boolean;
     };
-  }
-
-  interface User {
-    id: string;
-    role?: string | null;
-    isProfileComplete?: boolean | null;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id?: string;
-    role?: string;
+    id: string;
+    role: Role | null;
     isProfileComplete?: boolean;
   }
 }
