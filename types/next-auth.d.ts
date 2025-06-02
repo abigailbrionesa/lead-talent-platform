0199ba6f01c891eb0f73de122e197802b63b029e
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-01 21:01:16 -0500

    more types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -22,3 +22,49 @@ declare module "next-auth/jwt" {
     isProfileComplete?: boolean;
   }
 }
+
+export type FormValues = {
+  role: string;
+  name: string;
+  email: string;
+  image: string;
+  age: string;
+  phone: string;
+  chapter: string;
+  university_cycle: string;
+  lead_role: string;
+  career: string;
+  linkedin_url: string;
+  resume_url: string;
+  availability: string;
+  company: string;
+};
+
+export type UserInForm = {
+  id: string;
+  name: string;
+  email: string;
+  emailVerified: Date | null;
+  image: string | null;
+  password: string | null;
+  role: "MEMBER" | "RECRUITER" | "ADMIN" | string;
+  isProfileComplete: boolean;
+  member?: {
+    birthday: string;
+    skills: string[];
+    languages: string[];
+    phone: string;
+    chapter: string;
+    university_cycle: string;
+    lead_role: string;
+    career: string;
+    linkedin_url: string;
+    birthday:string;
+    resume_url: string;
+    availability: string;
+    submitted_at: Date;
+  };
+  recruiter?: {
+    company: string;
+  };
+};
