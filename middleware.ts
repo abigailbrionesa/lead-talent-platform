2eb106919236a41c7373ed32a222e4aec750429d
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-05 06:11:10 -0500

    update for supabase auth

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { auth } from '@/auth';
-import { NextResponse } from 'next/server';
-import authConfig from "./auth.config"
-import NextAuth from 'next-auth';
-export const { auth: middleware } = NextAuth(authConfig)
-
+import { type NextRequest } from 'next/server'
+import { updateSession } from './utils/supabase/middleware'
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
-
-
-export default auth((req) => {
-  const { nextUrl, method } = req;
-  const user = req.auth?.user;
-  if (!user) return NextResponse.next();
-  return NextResponse.next();
-});
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
