6dad476df9c9225e1510967da614382bc85f7736
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-05 06:14:27 -0500

    simple login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,33 +1,19 @@
-"use client"
-import { LoginForm } from "@/components/login-form"
-import { useSearchParams } from "next/navigation";
-
+import { Sign } from 'crypto'
+import { login } from './actions'
+import { signup } from './actions'
+import SignIn from '@/components/ui/sign-in'
+import SignOut from '@/components/ui/sign-out'
 export default function LoginPage() {
-  const searchParams = useSearchParams();
-  const error = searchParams.get("error");
-  
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            {/*LOGO*/}
-            LEAD
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <LoginForm error={error}/>
-          </div>
-        </div>
-      </div>
-      <div className="relative hidden bg-muted lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
-    </div>
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+      <SignIn/>
+      <SignOut/>
+    </form>
   )
 }
