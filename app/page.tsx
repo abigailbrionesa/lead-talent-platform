48e0a236d6d49b98056267ac88bcc363f44fee97
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-05 06:12:22 -0500

    use supabase auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,38 @@
-
 import { auth } from "@/auth";
 import { Navbar } from "@/components/global/navbar";
 import { LandingSection } from "@/components/sections/landing";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
 
 export default async function Home() {
-  const session = await auth();
+  const supabase = await createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  let profile = null;
+
+  if (session) {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", session.user.id)
+      .single();
+
+    if (!data || error) {
+      redirect("/account");
+    }
+
+    profile = data;
+  } else {
+    redirect("/login");
+  }
 
   return (
     <div>
-      <Navbar user={session?.user} />
-      <LandingSection user={session?.user} />
+      <Navbar user={profile} />
+      <LandingSection user={profile} />
     </div>
   );
 }
