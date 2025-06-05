e6ab0dc825524a508484222b52b84dcb1decfc40
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-05 06:15:48 -0500

    sign out with supabase

--- components/ui/sign-out.tsx
+++ components/ui/sign-out.tsx
@@ -1,10 +1,19 @@
 "use client";
-import { Button } from "./button";
-import { signOut } from "next-auth/react";
 
+import { Button } from "./button";
+import { createClient } from "@/utils/supabase/client"; 
 export default function SignOut() {
+  const supabase = createClient();
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error signing out:", error.message);
+    } else {
+      console.log("Signed out successfully");
+    }
+  };
   return (
-    <Button onClick={() => signOut()} size="sm" variant="ghost">
+    <Button onClick={handleSignOut} size="sm" variant="ghost">
       Cerrar Sesión
     </Button>
   );
