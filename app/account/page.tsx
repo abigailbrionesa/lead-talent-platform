388e2ecda93702d15f3191f61223619c61c6593d
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-05 06:13:09 -0500

    main page of account

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,28 +1,53 @@
-import prisma from "@/lib/prisma";
 import AccountForm from "./account-form";
-import { auth } from "@/auth";
 import { Navbar } from "@/components/global/navbar";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function AccountPage() {
-  const session = await auth();
-  if (!session) return <div>Not authenticated</div>
+  const supabase = await createClient();
 
-  const user = await prisma.user.findUnique({
-    where: { id: session.user.id },
-    include: {
-      member: true,
-      recruiter: true,
-    },
-  });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  if (!user) return <p>User not found</p>;
+  if (!user) {
+    return null;
+  }
+
+  const {} = await supabase
+    .from("profiles")
+    .upsert({ id: user.id, email: user.email, full_name: user.user_metadata.full_name, profile_picture: user.user_metadata.picture }, { onConflict: "id" });
+
+  const {
+    data: user_profile_data,
+  } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("id", user.id)
+    .single();
+
+  const {
+    data: user_member_data, //can be null
+  } = await supabase
+    .from("member_profiles")
+    .select("*")
+    .eq("id", user.id)
+    .single();
+
+  
+  const {
+    data: user_recruiter_data, //can be null
+  } = await supabase
+    .from("recruiter_profiles")
+    .select("*")
+    .eq("id", user.id)
+    .single();  
 
   return (
     <div>
-      <Navbar user={session?.user} />
+      <Navbar user={user_profile_data} />
       <div className="h-20" />
       <h1 className="text-2xl font-bold mb-6">Edit Profile</h1>
-        <AccountForm user={user} />
+      <AccountForm user_profile_data={user_profile_data} user_member_data={user_member_data} user_recruiter_data={user_recruiter_data} />
     </div>
   );
 }
