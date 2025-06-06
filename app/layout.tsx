a54bc417865b2723405df579cd8d1f1ee93c449c
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-06 09:51:27 -0500

    use layout to pass user info to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { createClient } from "@/utils/supabase/server";
 import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/global/navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,29 +20,35 @@ export const metadata: Metadata = {
   description: "comunidad LEAD",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+  const {data: {user}} = await supabase.auth.getUser();
+  let user_profile_data = null;
+  if (user) {
+    const { data } = await supabase
+      .from("profiles")
+      .select("role, profile_picture, full_name, email")
+      .eq("id", user.id)
+      .single();
+    user_profile_data = data;
+  }
   return (
     <html lang="en" suppressHydrationWarning>
-    <body
-        className={cn(
-          geistSans.variable,
-          geistMono.variable,
-          "antialiased",
-          "min-h-screen bg-background",
-        )}
-      >
+      <body className={cn(geistSans.variable,geistMono.variable,"antialiased","min-h-screen bg-background")}>
+        <Navbar user={user_profile_data} />
+        {/*CAN BE NULL  navbar needs the following elements (IT CAN BE NULL TOO) ROLE, PROFILE_PICTURE, FULL_NAME, EMAIL */}
         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
