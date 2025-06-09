b3638b82e14fb10f84856e500d9db8c6692fa94c
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-09 15:29:15 -0500

    simplify

--- app/login/actions.tsx
+++ app/login/actions.tsx
@@ -2,40 +2,16 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
-export async function login(formData: FormData) {
-  const supabase = await createClient()
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
-    console.log("Trying to login with:", data.email);
 
-  const { error } = await supabase.auth.signInWithPassword(data)
+export async function signIn() {
+  const supabase = await createClient()
+
+  const { error } = await supabase.auth.signInWithOAuth({provider: 'google'})
+
   if (error) {
-    console.error("Login error:", error); 
+    console.error("sign in error:", error); 
     redirect('/error')
   }
   revalidatePath('/', 'layout')
-    console.log("successfu l Login")
+}
 
-  redirect('/')
-}
-export async function signup(formData: FormData) {
-  const supabase = await createClient()
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
-  const { error } = await supabase.auth.signUp(data)
-  if (error) {
-    console.error("Login error:", error);
-    redirect('/error')
-  }
-  revalidatePath('/', 'layout')
-  console.log("successfu l singup")
-  redirect('/')
-}
