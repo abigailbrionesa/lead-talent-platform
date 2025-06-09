eaac4e8c1b689497f30092c56e76910800f9cca5
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-09 10:02:25 -0500

    update typescript

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,70 +1,8 @@
-import NextAuth from "next-auth";
-import type { Role } from "@prisma/client";
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      name: string;
-      email: string;
-      image: string | null;
-      role: Role | null;
-      emailVerified: Date | null;
-      password: string | null;
-      isProfileComplete: boolean;
-    };
-  }
-}
+export type platform_role = 'member' | 'recruiter' | 'admin' | null;
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    id: string;
-    role: Role | null;
-    isProfileComplete?: boolean;
-  }
-}
-
-export type FormValues = {
-  role: string;
-  name: string;
+export type nav_user_type = {
+  role: platform_role;
+  profile_picture: string;
+  full_name: string;
   email: string;
-  image: string;
-  age: string;
-  phone: string;
-  chapter: string;
-  university_cycle: string;
-  lead_role: string;
-  career: string;
-  linkedin_url: string;
-  resume_url: string;
-  availability: string;
-  company: string;
-};
-
-export type UserInForm = {
-  id: string;
-  name: string;
-  email: string;
-  emailVerified: Date | null;
-  image: string | null;
-  password: string | null;
-  role: "MEMBER" | "RECRUITER" | "ADMIN" | string;
-  isProfileComplete: boolean;
-  member?: {
-    birthday: string;
-    skills: string[];
-    languages: string[];
-    phone: string;
-    chapter: string;
-    university_cycle: string;
-    lead_role: string;
-    career: string;
-    linkedin_url: string;
-    birthday:string;
-    resume_url: string;
-    availability: string;
-    submitted_at: Date;
-  };
-  recruiter?: {
-    company: string;
-  };
-};
+}
