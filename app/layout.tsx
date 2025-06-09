e762ecb264730a76a148d8f89ef91170ae2f6dd7
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-09 09:56:10 -0500

    use navbar server

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
-import { createClient } from "@/utils/supabase/server";
 import { cn } from "@/lib/utils";
-import { Navbar } from "@/components/global/navbar";
+import NavbarServer from "@/components/global/navbar-server";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,22 +24,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const supabase = await createClient();
-  const {data: {user}} = await supabase.auth.getUser();
-  let user_profile_data = null;
-  if (user) {
-    const { data } = await supabase
-      .from("profiles")
-      .select("role, profile_picture, full_name, email")
-      .eq("id", user.id)
-      .single();
-    user_profile_data = data;
-  }
+ 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn(geistSans.variable,geistMono.variable,"antialiased","min-h-screen bg-background")}>
-        <Navbar user={user_profile_data} />
-        {/*CAN BE NULL  navbar needs the following elements (IT CAN BE NULL TOO) ROLE, PROFILE_PICTURE, FULL_NAME, EMAIL */}
+        <NavbarServer />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
