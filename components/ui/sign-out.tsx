3db8f4f25bd633549e0e981b33020c9c704b4049
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-09 15:31:52 -0500

    log out

--- components/ui/sign-out.tsx
+++ components/ui/sign-out.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "./button";
 import { createClient } from "@/utils/supabase/client"; 
+import { LogOut } from "lucide-react";
+
 export default function SignOut() {
   const supabase = createClient();
   const handleSignOut = async () => {
@@ -9,11 +11,12 @@ export default function SignOut() {
     if (error) {
       console.error("Error signing out:", error.message);
     } else {
-      console.log("Signed out successfully");
+      console.log("Signed out successfully from Supabase Auth");
     }
   };
   return (
     <Button onClick={handleSignOut} size="sm" variant="ghost">
+      <LogOut />
       Cerrar Sesión
     </Button>
   );
